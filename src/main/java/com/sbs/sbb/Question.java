d759206c73d7364b614fcda182565798c3630e7d
seongghun <esd2727@gmail.com>
2024-05-07 11:43:40 +0900

    jpa연결, question테이블 생성

---
+++ src/main/java/com/sbs/sbb/Question.java
@@ -0,0 +1,23 @@
+package com.sbs.sbb;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity // question 테이블
+public class Question {
+    @Id // Primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Integer id;
+
+    @Column(length = 200) // VARCHAR(200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT") // TEXT
+    private String content;
+
+    private LocalDateTime createDate;
+}
