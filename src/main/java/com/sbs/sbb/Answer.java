1776c7e6f1bb82b3848ba629d3034acf24a7a602
seongghun <esd2727@gmail.com>
2024-05-07 11:53:45 +0900

    ãAnswer 추가

---
+++ src/main/java/com/sbs/sbb/Answer.java
@@ -0,0 +1,25 @@
+package com.sbs.sbb;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity // answer 테이블
+public class Answer {
+    @Id // Primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT") // TEXT
+    private String content;
+
+    private LocalDateTime createDate;
+
+    // private Integer questionId;
+    @ManyToOne
+    private Question question;
+    // question_id 라는 칼럼이 생김
+}
